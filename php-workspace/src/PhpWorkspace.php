2c6a9ed44b7a2eb5d0a5e2f1991eb3e4ec3b0fa5
Jason McCallister <jason@mccallister.dev>
2025-03-06 10:04:27 -0500

    Add workspace and refactor
    Signed-off-by: Jason McCallister <jason@mccallister.dev>

---
+++ php-workspace/src/PhpWorkspace.php
@@ -0,0 +1,61 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DaggerModule;
+
+use Dagger\Attribute\DaggerFunction;
+use Dagger\Attribute\DaggerObject;
+use Dagger\Attribute\Doc;
+use Dagger\Container;
+use Dagger\Directory;
+use Dagger\File;
+
+use function Dagger\dag;
+
+#[DaggerObject]
+class PhpWorkspace
+{
+    public Container $container;
+
+    #[DaggerFunction]
+    public function __construct(
+        #[Doc("The source directory.")] private readonly Directory $source
+    ) {
+        $this->container = dag()
+            ->php()
+            ->setup()
+            ->withWorkdir("/app")
+            ->withMountedDirectory("/app", $this->source)
+            ->withMountedDirectory(
+                "/app/vendor",
+                dag()->composer(source: $this->source)->install()
+            )
+            ->withDefaultTerminalCmd(["/bin/bash"]);
+    }
+
+    #[DaggerFunction]
+    public function diff(): string
+    {
+        return $this->container->withExec(["git", "diff"])->stdout();
+    }
+
+    #[DaggerFunction]
+    public function read(string $path): string
+    {
+        return $this->container->file($path)->contents();
+    }
+
+    #[DaggerFunction]
+    public function test(): string
+    {
+        return $this->container->withExec(["php", "artisan", "test"])->stdout();
+    }
+
+    #[DaggerFunction]
+    #[Doc("Writes a file to the container.")]
+    public function write(string $path, string $contents): File
+    {
+        return $this->container->withNewFile($path, $contents)->file($path);
+    }
+}
--- src/LaravelAssistant.php
+++ src/LaravelAssistant.php
@@ -6,52 +6,35 @@ namespace DaggerModule;
 
 use Dagger\Attribute\DaggerFunction;
 use Dagger\Attribute\DaggerObject;
-use Dagger\Attribute\DefaultPath;
 use Dagger\Attribute\Doc;
 use Dagger\Container;
 use Dagger\Directory;
 
-use Dagger\File;
 use function Dagger\dag;
 
 #[DaggerObject]
 #[Doc("A LLM module to assist with writing Laravel tests.")]
 class LaravelAssistant
 {
-    private Container $container;
-
     #[DaggerFunction]
     public function __construct(
         #[Doc("The source directory.")] private readonly Directory $source
-    ) {
-        $this->container = dag()
-            ->php()
-            ->setup()
-            ->withWorkdir("/app")
-            ->withMountedDirectory("/app", $this->source)
-            ->withMountedDirectory(
-                "/app/vendor",
-                dag()->composer(source: $this->source)->install()
-            );
-    }
+    ) {}
 
     #[DaggerFunction]
     #[Doc("Assists with writing tests.")]
-    public function assist(): Container
+    public function writeTests(): Container
     {
+        $before = dag()->phpWorkspace($this->source);
+
         $after = dag()
             ->llm()
-            ->withContainer($this->container)
-            ->withPromptVar("diff", $this->diff())
+            ->withPhpWorkspace($before)
             ->withPrompt(
                 <<<EOT
 You are an expert in Laravel. You understand the framework and its ecosystem. You have a deep understanding of the Laravel lifecycle and can build complex applications with ease. You are comfortable with the command line and can navigate the Laravel directory structure with ease.
 
-Using the diff below, determine the type of test to create and provide a complete example of the test. Only consider PHP files in the directory for this task. You prefer feature tests over unit tests.
-
-<diff>
-\$diff
-</diff>
+Examine the source code and write tests that are missing and ensure all tests pass.
 
 Use the write tool to put the complete test file. Before using the write tool, use the test tool to ensure tests are passing.
 
@@ -59,27 +42,6 @@ Don't stop until your tests pass.
 EOT
             );
 
-        return $after->Container();
-    }
-
-    #[DaggerFunction]
-    #[Doc("Returns the diff of the source directory.")]
-    public function diff(): string
-    {
-        return $this->container->withExec(["git", "diff"])->stdout();
-    }
-
-    #[DaggerFunction]
-    #[Doc("Runs the tests.")]
-    public function test(): string
-    {
-        return $this->container->withExec(["php", "artisan", "test"])->stdout();
-    }
-
-    #[DaggerFunction]
-    #[Doc("Writes a file to the container.")]
-    public function write(string $path, string $contents): File
-    {
-        return $this->container->withNewFile($path, $contents)->file($path);
+        return $after->container();
     }
 }
